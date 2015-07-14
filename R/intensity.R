ecb1aa13aa0eef7d2698e91796c4d30da14b4394
chenjiaxun9 <chenjiaxun9@hotmail.com>
2015-07-14 14:06:28 -0500

    Nonparametric intensity estimation

---
+++ R/intensity.R
@@ -0,0 +1,44 @@
+est_intenstiy_np <- function(pattern, win, h, kernel=c("Epanechnikov"),
+                             edgecorrect=TRUE, truncate=TRUE, L=10){
+  kernel <- match.arg(kernel)
+  if (truncate==TRUE) {
+    pattern <- pattern[spatstat::inside.owin(pattern$x,pattern$y,win)]
+  }
+  x <- seq(win$xrange[1],win$xrange[2],length.out = L)
+  y <- seq(win$yrange[1],win$yrange[2],length.out = L)
+  lenx <- length(x)
+  leny <- length(y)
+  intensity <- matrix(0,lenx,leny)
+  loc <- expand.grid(x,y)
+  pp <- cbind(pattern$x,pattern$y)
+  index <- expand.grid(1:pattern$n, 1:(lenx*leny))
+  xy <- loc[index[,2],]-pp[index[,1],]
+  if (edgecorrect==TRUE){
+    LL <- 10
+    ax <- seq(win$xrange[1],win$xrange[2], length.out = LL)
+    ay <- seq(win$yrange[1],win$yrange[2], length.out = LL)
+    alenx <- length(ax)
+    aleny <- length(ay)
+    area <- (ax[2] - ax[1]) * (ay[2] - ay[1])
+    centers <- expand.grid(x_center=(ax[1:(alenx-1)] + ax[2:alenx])/2,
+                           y_center=(ay[1:(aleny-1)] + ay[2:aleny])/2)
+    index.mass <- expand.grid(1:((alenx-1)*(aleny-1)),1:(lenx*leny))
+    dist.centers <- loc[index.mass[,2],]-centers[index.mass[,1],]
+    if(kernel=="Epanechnikov"){
+      diff <- rowSums((dist.centers)^2)
+      mass <- ifelse(diff < 1, 4*(1-diff) / (2*pi), 0)
+      edgecor <- area * aggregate(mass,list(index.mass[,2]),sum) / (h^2)
+      quad <- rowSums((xy/h)^2)
+      val <- ifelse(quad < 1, 4*(1 - quad) / (2*pi), 0)
+      intensity <- aggregate(val, list(index[, 2]), sum) / (h^2*edgecor)
+    }
+  } else {
+    if(kernel=="Epanechnikov"){
+      quad <- rowSums((xy/h)^2)
+      val <- ifelse(quad < 1, 4*(1 - quad) / (2*pi), 0)
+      intensity <- aggregate(val, list(index[, 2]), sum) / h^2
+    }
+  }
+  return(im(matrix(intensity[,2],lenx,leny,byrow=T),x,y))
+}
+
