410833db6617ec042e05f9ae5ddea6df7d76de3e
Yuchen Wang <ycwang0712@gmail.com>
2015-07-20 09:53:34 -0500

    add docs

--- R/random_num_generator.R
+++ R/random_num_generator.R
@@ -1,23 +1,11 @@
-# rbivnormal <- function(n, mu = 0, Sigma = diag(2)) {
-#   z <- rnorm(2 * n)
-#   decomp <- chol(Sigma)
-#   x <- mu + t(decomp) %*% matrix(z, 2, n)
-#   return(t(x))
-# }
-#
-# dbivnormal <- function(x, mu = 0, Sigma = diag(2)) {
-#   exp(-.5 * t(x - mu) %*% solve(Sigma) %*% (x - mu)) / sqrt(2 * pi * det(Sigma))
-# }
-#
-# rwishart <- function(n, Sigma) {
-#   x <- rbinorm(n, 0, Sigma)
-#   xxp <- list()
-#   for (i in 1:n) {
-#     xxp[[i]] <- x[i, ] %*% t(x[i, ])
-#   }
-#   return(Reduce("+", xxp))
-# }
-
+#' Dirichlet random number generator.
+#'
+#' Generate random numbers that follow Dirichlet distribution.
+#'
+#' @param n Number of generator
+#' @param alpha Parameter for Dirichlet distribution
+#'
+#'
 rdirichlet <- function (n, alpha) {
   l <- length(alpha)
   x <- matrix(rgamma(l * n, alpha), ncol = l, byrow = TRUE)
