ab2450b1c23d05076e0e124d18088e16f3e50091
chenjiaxun9 <chenjiaxun9@hotmail.com>
2015-08-08 11:20:09 -0500

    model selection V1

---
+++ R/select_mix_model.R
@@ -0,0 +1,34 @@
+selectMix <- function(pattern, win, Ms, L = 1000,
+                        burnin = 200, truncate = TRUE){
+  if (truncate==TRUE) {
+    pattern <- pattern[spatstat::inside.owin(pattern$x, pattern$y, win)]
+  }
+
+  pp <- as.data.frame(pattern)
+  n <- npoints(pattern)
+
+  if (L <= burnin) {
+    stop("wrong L or burnin")
+  }
+  window_area <- diff(win$x) * diff(win$y)
+  lambdahat <- n / window_area
+  mmax <- length(Ms)
+  AIC <- rep(0, mmax)
+  BIC <- rep(0, mmax)
+  marginal <- rep(0, mmax)
+  for (m in 1:mmax) {
+   mix <- est_mix_intensity(pattern, win, m = Ms[m], marginal = TRUE)
+   loglikelihood <- n*log(mix$lambda) - mix$lambda
+   den <- dnormmix(mix$post_mix,win = win)$v
+   loglikelihood <- loglikelihood + sum(log(den))
+   r <- 1 + 6 * Ms[m]
+   AIC[m] <- 2 * r - 2 * loglikelihood
+   BIC[m] <- r * log(n) - 2 * loglikelihood
+   marginal[m] <- mix$marginal
+  }
+  index <- as.matrix(expand.grid(1:mmax,1:mmax))
+  bayes_factor <- apply(index,1,function(x) marginal[x[2]]/marginal[x[1]])
+  RVAL <- list(AIC = AIC,
+               BIC = BIC,
+               bayes_factor = bayes_factor)
+}
