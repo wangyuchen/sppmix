e62ab28098092248c7f416fdadfabee7e533ecf1
chenjiaxun9 <chenjiaxun9@hotmail.com>
2015-10-24 00:41:53 -0500

    Add summary function for DAMCMC ouput

---
+++ R/summary_post.R
@@ -0,0 +1,32 @@
+#' @export
+summary.damcmc_res <- function(gens) {
+  m = dim(gens$genmus)[1]
+  for (i in 1:m)
+  {
+    #true value and credible sets
+    poststats = GetStats_sppmix(gens$genps[,i],alpha=0.05)
+    cat("\n----------------Component ",i,"------------\n")
+    #cat(paste("Probability: true =",truemix[[i]]$p))
+    cat(paste("\nProbability: posterior mean =",
+              round(poststats$Mean,  digits = 4),"\n"))
+    cat(paste(poststats$CredibleSetConfidence,
+              "% Credible Set:\n[",poststats$CredibleSet[1],
+              ",",poststats$CredibleSet[2],"]" ))
+    poststas = GetStats_sppmix(gens$genmus[i,1,],alpha=0.05)
+    #cat(paste("\nMean vector, x-coord: true =",truemix[[i]]$mu[1]))
+    cat(paste("\nMean vector, x-coord: post mean =",
+              round(poststats$Mean, digits = 4),"\n"))
+    cat(paste(poststats$CredibleSetConfidence,
+              "% Credible Set:\n[",poststats$CredibleSet[1],
+              ",",poststats$CredibleSet[2],"]" ))
+    poststats = GetStats_sppmix(gens$genmus[i,2,],alpha=0.05)
+    #cat(paste("\nMean vector, y-coord: true =",truemix[[i]]$mu[2]))
+    cat(paste("\nMean vector, y-coord: post mean =",
+              round(poststats$Mean, digits = 4),"\n"))
+    cat(paste(poststats$CredibleSetConfidence,
+              "% Credible Set:\n[",poststats$CredibleSet[1],
+              ",",poststats$CredibleSet[2],"]" ))
+  }
+  cat("\n----------------Component stats done------------\n")
+  #cat("\nNOTE: if you have the truth, then what is called comp
+}
