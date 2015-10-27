df1ba48b9d3c8b8e7002c75dbae7b2e306257234
chenjiaxun9 <chenjiaxun9@hotmail.com>
2015-10-27 17:49:21 -0500

    remove burnin

--- R/est_mix_intensity.R
+++ R/est_mix_intensity.R
@@ -42,13 +42,13 @@ get_post.damcmc_res <- function(fit, burnin) {
 #' @export
 est_mix_bdmcmc <- function(pp, max_comp, truncate = FALSE,
                            lambda, lambdab, hyper,
-                           L = 5000, burnin = 500, LL = 100) {
+                           L = 5000, LL = 100) {
 
   fit <- BDMCMC2d_sppmix(max_comp, data = cbind(pp$x, pp$y),
                          xlims = Window(pp)$xrange, ylims = Window(pp)$yrange,
                          truncate = truncate,
                          lamda = lambda, lamdab = lambdab, hyper = hyper,
-                         L = L, burnin = burnin, LL = LL)
+                         L = L, LL = LL)
   class(fit) <- "bdmcmc_res"
   return(invisible(fit))
 }
