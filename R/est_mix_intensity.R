82a2379a0b1bdd0b873a53dedb09466369560c67
Yuchen Wang <ycwang0712@gmail.com>
2015-11-22 22:38:18 -0500

    BDMCMC

--- R/est_mix_intensity.R
+++ R/est_mix_intensity.R
@@ -1,3 +1,20 @@
+#' Estimate mixture model using MCMC
+#'
+#' These functions fits a mixture model to spatial point pattern data using
+#' DAMCMC or BDMCMC.
+#'
+#' @param pp point pattern object of class \code{ppp}.
+#' @param m either number of components to fit in data augmentation MCMC or
+#' maximum number of component in Birth Death MCMC.
+#' @param truncate logical, indicating whether truncation is used, where the
+#' component density are restricted within the domain of the point pattern.
+#' @param L length of MCMC chain, default to 5000.
+#' @param LL additional fitting parameter
+#'
+#' @rdname est_mix
+#' @examples
+#' # see vignett "Work with 2D Normal Mixtures" for details.
+#'
 #' @export
 est_mix_damcmc <- function(pp, m, truncate = FALSE,
                            L = 5000, LL = 100) {
@@ -11,8 +28,18 @@ est_mix_damcmc <- function(pp, m, truncate = FALSE,
   return(invisible(fit))
 }
 
-
+#' Calculate posterior mixture from estimated mixture
+#'
+#' @param fit object from \code{est_mix_damcmc} or \code{est_mix_bdmcmc}.
+#' @param burnin number of burn-in iterations.
+#'
 #' @export
+#' @examples
+#'
+#' fit <- sppmix::est_mix_damcmc(pp = redwood, m = 3, truncate = FALSE,
+#'                               L = 50000, LL = 100)
+#' get_post(fit, burnin = 5000)
+#'
 get_post <- function(obj, ...) {
   UseMethod("get_post", obj)
 }
@@ -23,11 +50,11 @@ get_post.damcmc_res <- function(fit, burnin) {
   m <- ncol(fit$genps)
   if (missing(burnin)) burnin <- L / 10
 
-  post_ps <- colMeans(fit$genps[-(1:burnin), ])
-  mus <- apply(fit$genmus[, , -(1:burnin)], 1:2, mean)
+  post_ps <- colMeans(fit$genps[-(1:burnin), , drop = FALSE])
+  mus <- apply(fit$genmus[, , -(1:burnin), drop = FALSE], 1:2, mean)
 
   mean_mat <- function(mats) Reduce("+", mats) / length(mats)
-  sigmas <- apply(fit$gensigmas[-(1:burnin), ], 2, mean_mat)
+  sigmas <- apply(fit$gensigmas[-(1:burnin), , drop = FALSE], 2, mean_mat)
 
   mean_lambda <- mean(fit$genlamdas)
 
@@ -41,17 +68,58 @@ get_post.damcmc_res <- function(fit, burnin) {
               mean_lambda = mean_lambda))
 }
 
-
+#' @param lambda parameter for BDMCMC
+#' @param lambdab parameter for BDMCMC
+#' @param hyper parameter for BDMCMC
+#' @rdname est_mix
 #' @export
-est_mix_bdmcmc <- function(pp, max_comp, truncate = FALSE,
+est_mix_bdmcmc <- function(pp, m, truncate = FALSE,
                            lambda, lambdab, hyper,
                            L = 5000, LL = 100) {
 
-  fit <- BDMCMC2d_sppmix(max_comp, data = cbind(pp$x, pp$y),
+  fit <- BDMCMC2d_sppmix(m, data = cbind(pp$x, pp$y),
                          xlims = Window(pp)$xrange, ylims = Window(pp)$yrange,
                          truncate = truncate,
                          lamda = lambda, lamdab = lambdab, hyper = hyper,
                          L = L, LL = LL)
+  fit$data <- pp
   class(fit) <- "bdmcmc_res"
   return(invisible(fit))
 }
+
+
+
+
+#' @export
+get_post.bdmcmc_res <- function(fit, burnin) {
+#   L <- length(fit$allgens)
+#   m <- ncol(fit$genps)
+#   if (missing(burnin)) burnin <- L / 10
+#
+#   post_ps <- colMeans(fit$genps[-(1:burnin), , drop = FALSE])
+#   mus <- apply(fit$genmus[, , -(1:burnin), drop = FALSE], 1:2, mean)
+#
+#   mean_mat <- function(mats) Reduce("+", mats) / length(mats)
+#   sigmas <- apply(fit$gensigmas[-(1:burnin), , drop = FALSE], 2, mean_mat)
+#
+#   mean_lambda <- mean(fit$genlamdas)
+#
+#   post_mus <- post_sigmas <- vector("list", m)
+#   for (i in 1:m) {
+#     post_mus[[i]] <- mus[i, ]
+#     post_sigmas[[i]] <- matrix(sigmas[, i], 2, 2)
+#   }
+#
+#   return(list(post_normmix = normmix(post_ps, post_mus, post_sigmas),
+#               mean_lambda = mean_lambda))
+}
+
+
+
+
+
+
+
+
+
+
