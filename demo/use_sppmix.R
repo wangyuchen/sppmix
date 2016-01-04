7670bdc755cf621062598333331e3ad938b95cc3
chenjiaxun9 <chenjiaxun9@hotmail.com>
2016-01-03 22:31:30 -0600

    demo version 1

--- demo/use_sppmix.R
+++ demo/use_sppmix.R
@@ -1,43 +1,42 @@
 library(MASS)
-# mu=c(0,0)
-# sig=rbind(c(1,0),c(0,1))
-#rnorm_cpp(n,mu,sig)
-#  m <- diag(2)
-#matrix(rnorm(4), 2, 2)
-#getEigenValues(m)
-#mvrnormArma(10,c(0,0),diag(2))
-# microbenchmark(DAMCMC2d(n,mu,sig))
-#  data
 if(Get_User_Input_sppmix("Use default parameter values?"))
 {
-  xlims<<-c(0,10)
-  ylims<<-c(0,10)
-  L<<-5000
-  m<<-3
-  burnin<<-1000
-  lamda<<-100
-  r<<-30
-  LL<<-50
-  maxnumcomp<<-10
+  xlims <- c(0,10)
+  ylims <- c(0,10)
+  win <- square(10)
+  L <- 5000
+  m <- 3
+  burnin <- 1000
+  lamda <- 100
+  sig0 <- 0.1
+  sigdf <- 5
+  LL <- 50
+  maxnumcomp <- 10
 }else
 {
-  m<<-Get_User_Input_sppmix("the true number of components",modeYN=0)
-  xlims<<-c(
+  m <- Get_User_Input_sppmix("the true number of components",modeYN=0)
+  xlims <- c(
     Get_User_Input_sppmix("minx",modeYN=0),
     Get_User_Input_sppmix("maxx",modeYN=0))
-  ylims<<-c(
+  ylims <- c(
     Get_User_Input_sppmix("miny",modeYN=0),
     Get_User_Input_sppmix("maxy",modeYN=0))
-  L<<-Get_User_Input_sppmix("number of iterations",modeYN=0)
-  burnin<<-Get_User_Input_sppmix("burnin",modeYN=0)
-  lamda<<-Get_User_Input_sppmix("lambda (affects number of events)",modeYN=0)
-  r<<-Get_User_Input_sppmix("parameter r (affects the cov matrices of components)",modeYN=0)
+  win <- owin(xlims,ylims)
+  L <- Get_User_Input_sppmix("number of iterations",modeYN=0)
+  burnin <- Get_User_Input_sppmix("burnin",modeYN=0)
+  lamda <- Get_User_Input_sppmix("lambda (affects number of events)",modeYN=0)
+  sig0 <- Get_User_Input_sppmix(
+    "Tunning parameter in generating random matrix from Wishart distribution)",
+    modeYN=0)
+  sigdf <- Get_User_Input_sppmix(
+    "Degree of freedom in generating random matrix from Wishart distribution.",
+    modeYN = 0)
   #    if(Get_User_Input_sppmix("Apply truncation to the window specified (results in slow operations)"))
-  #      truncated<<-TRUE
+  #      truncated <- TRUE
   #    else
-  #      truncated<<-FALSE
-  LL<<-Get_User_Input_sppmix("grid side length (the larger the slower the operations but much prettier...)",modeYN=0)
-  maxnumcomp<<-Get_User_Input_sppmix("maximum number of components for BDMCMC",modeYN=0)
+  #      truncated <- FALSE
+  LL <- Get_User_Input_sppmix("grid side length (the larger the slower the operations but much prettier...)",modeYN=0)
+  maxnumcomp <- Get_User_Input_sppmix("maximum number of components for BDMCMC",modeYN=0)
 }
 # data=rnorm2_sppmix(n,mu,sig)
 #  Plots_off()
@@ -46,65 +45,81 @@ if(Get_User_Input_sppmix("Remove all plots?"))
   Plots_off()
 
 if(Get_User_Input_sppmix("Apply truncation?"))
-  truncated<<-TRUE else  truncated<<-FALSE
+  truncated <- TRUE else  truncated <- FALSE
 
 if(Get_User_Input_sppmix("Generate the true mixture?"))
-  truemix<<-GenNormalMixture(lamda,m,xlims,ylims,r,truncated)
+  #truemix <- GenNormalMixture(lamda,m,xlims,ylims,r,truncated)
+  mix_demo <- rnormmix(m, sig0, sigdf, win)
+  truemix <- rsppmix(lamda, mix = mix_demo, truncate = truncated, win = win)
+  true_mus <- data.frame(matrix(unlist(mix_demo$mus), m, 2, byrow = TRUE))
+  names(true_mus) <- c("x","y")
+  plot(truemix)+ggplot2::geom_point(data = true_mus, size = 3, colour ="red")
 
 if(Get_User_Input_sppmix("Simulate from the posterior (DAMCMC)?"))
-  gens<<-DAMCMC2d_sppmix(gendata,xlims,ylims,m,L,LL,trunc=truncated)
-
+  gens <- est_mix_damcmc(truemix, m = m, L = L, truncate = truncated)
+  post_demo <- get_post(gens, burnin = burnin)
 #  cat("passed")
 if(Get_User_Input_sppmix("Show basic 2d and 3d plots?"))
 {
-  zmax_truemix=lamda*GetMixtureMaxz_sppmix(truemix,
-                                           100,xlims,ylims);
-  mix_of_postmeans<<-#MakeMixtureList_sppmix(
-    MakeMixtureList(gens$allgens_List,burnin)
-  mean_lambda<<-mean(gens$genlamdas[burnin:L]);
-  zmax_genmeanmix=mean_lambda *
-    GetMixtureMaxz_sppmix(mix_of_postmeans,
-                          100,xlims,ylims);
-  #find the highest z
-  maxz_height<<-max(c(zmax_truemix,zmax_genmeanmix))
-  #do all the plotting with a common maximum z value
-  PlotNormalMixture(mix1=truemix,data1=gendata,
-                    m1=m,lamda1=lamda,xlims1=xlims,
-                    ylims1=ylims,L1=100,
-                    title1="True mixture",zlims1=c(0,1.1*maxz_height),
-                    title3d=paste("True mixture intensity surface,",m,"components,",n,"points"))
+  zmax_truemix <- max(lamda*dnormmix(mix_demo,win));
+  zmax_genmeanmix <- max(post_demo$mean_lambda*dnormmix(post_demo$post_normmix,
+                                                        win))
+  zmax <- max(c(zmax_truemix, zmax_genmeanmix))
 
-  PlotNormalMixture(mix1=mix_of_postmeans,
-                    data1=gendata,
-                    m1=m,lamda1=mean_lambda,xlims1=xlims,
-                    ylims1=ylims,
-                    L1=100,title1="Posterior means",zlims1=c(0,1.1*maxz_height),
-                    title3d=paste("Intensity surface of posterior means,",m,"components,",n,"points"))
+  plot(mix = mix_demo, lambda = lamda, win = win,
+       zlims = c(0, 1.1*zmax), truncate = truncated,
+       title1 = paste("Intensity surface of the true model with,",m,
+                      "components,",truemix$n,"points"))
+  plot(post_demo$post_normmix, post_demo$mean_lambda, win = win,
+       zlims = c(0, 1.1*zmax), truncate = truncated,
+       title1 = paste("Intensity surface of posterior means,",m,
+                      "components,",truemix$n,"points"))
+#   PlotNormalMixture(mix1=mix_of_postmeans,
+#                     data1=gendata,
+#                     m1=m,lamda1=mean_lambda,xlims1=xlims,
+#                     ylims1=ylims,
+#                     L1=100,title1="Posterior means",zlims1=c(0,1.1*maxz_height),
+#                     title3d=paste("Intensity surface of posterior means,",m,
+#                                   "components,", truemix$n,"points"))
 }
 if(Get_User_Input_sppmix("Show Chains and Stats?"))
 {
   #  plot_ind(gens)
-  ShowChains(gens$genps,gens$genmus)
-  ShowStats(gens$genps,gens$genmus,truemix)
+  plot_chains(gens, burnin = burnin)
+  summary(gens)
 }
 
 if(Get_User_Input_sppmix("Check for label switching?"))
-  CheckLabels(gens$genmus[,,burnin:L])
+  test_labswitch(gens$genmus[,,burnin:L])
 
 if(Get_User_Input_sppmix("Show average of intensity surfaces (slow operation)?"))
-  Show3dAvgofsurfaces(gens,LL,burnin,xlims,ylims,zlims=c(0,1.1*maxz_height))
+  plot_avgsurf(gens, win = win, burnin = burnin, LL = 60, zlims=c(0,1.1*zmax))
 
 if(Get_User_Input_sppmix("Apply relabeling algorithm?"))
 {
-  FixLabels(gens,data1=gendata,truemix,1.1*maxz_height,
-            m1=m,xlims1=xlims,ylims1=ylims)
+  perm_post <- FixLS_da(gens, plot_result = TRUE)
 }
 #  sppmix::BDMCMC2d_sppmix(20,gendata,xlims,ylims,L,31,FALSE,1,20,c(15,.01,3,2,1,1))
 
 if(Get_User_Input_sppmix("Run the Birth-Death MCMC fit?"))
 {
-  gensBD<<-BDMCMC2d_sppmix(maxnumcomp,gendata,xlims,ylims,L,LL,FALSE,1,10,c(5,.01,3,2,1,1))
+  gensBD <- est_mix_bdmcmc(pp = truemix, m = m, truncate = truncated,
+                           lambda = 1, lambdab = 10,
+                           hyper = c(5,.01,3,2,1,1), L = L)
   if(Get_User_Input_sppmix("Show Birth-Death MCMC plots?"))
-    PostGenBDMCMC_sppmix(gensBD,maxz_height)
+    cat("Frequency table for the number of components")
+  print(table(gensBD$numcomp))
+  tab=tabulate(gensBD$numcomp,nbins=gensBD$maxnumcomp)
+    mp <- barplot(tab,names.arg=1:gensBD$maxnumcomp,
+                 xlab="Number of Components",ylab="Iterations",
+                 main="Distribution of the number of components"
+                 ,ylim=c(0,1.2*max(tab)))
+    plot(gensBD$numcomp, xlab="Iteration", ylab="Number of components",
+         type="l", main="Generated chain for the number of components")
+    postBD <- get_post(gensBD, comp =which.max(tab), burnin = burnin)
+    plot(postBD$post_normmix, postBD$mean_lambda, win = win,
+         zlims = c(0, 1.1*zmax), truncate = truncated,
+         title1 = paste("Intensity surface of posterior means, MAP m=,",m,
+                        "components,",truemix$n,"points"))
 }
 
