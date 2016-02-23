243f1621bab822732f3b6bef227baa23d365a8b3
Yuchen Wang <ycwang0712@gmail.com>
2016-02-23 00:34:14 -0500

    update intensity surface setup

--- R/normmix.R
+++ R/normmix.R
@@ -5,32 +5,40 @@
 #' @param ps Vector of component probabilities.
 #' @param mus A list where every element is a vector of length 2, defining the
 #'  center of each component.
-#' @param sigmas A list where every element is a 2 by 2 matrix, defining the
-#'  covariance of each component.
-#' @param lambda optional parameter of average intensity.
-#' @param mix An object of class \code{\link{normmix}}
+#' @param sigmas A list where every element is a 2 by 2 covariance matrix,
+#' defining the covariance for each component.
+#' @param lambda optional parameter of average intensity. If set, the returned
+#' object will be an intensity surface.
+#' @param win optional parameter of domain. Must be set with intensity value to
+#' create an intensity surface.
 #'
 #' @return An object of class "normmix" containing the following components:
 #'  \item{m}{Number of components.}
 #'  \item{ps}{Vector of component probabilities.}
 #'  \item{mus}{List of mean vectors of components.}
 #'  \item{sigmas}{List of covariance matrix of components.}
+#'  \item{intensity}{optional intensity value if lambda is provided when calling
+#'  normmix().}
+#'  \item{window}{optional window object of class \code{\link{spatstat::owin().}
+#'
 #' @seealso \code{\link{rnormmix}} for generating random mixture.
 #' @export
 #' @examples
-#' mix1 <- normmix(ps=c(.3, .7), mus=list(c(0, 0), c(1, 1)),
-#'                 sigmas=list(.01*diag(2), .01*diag(2)))
+#' mix1 <- normmix(ps = c(.3, .7),
+#'                 mus = list(c(0.2, 0.2), c(.8, .8)),
+#'                 sigmas = list(.01*diag(2), .01*diag(2)))
 #' mix1
-#' summary(mix1)
-#' if (require(spatstat)) {
-#'   plot(mix1, square(1))
-#' }
-normmix <- function(ps, mus, sigmas, lambda) {
-  if (length(ps) == 0) {
-    stop("Mixture with 0 components")
-  }
-
-  if (abs(sum(ps) - 1) > .0000001) {
+#'
+#'
+#' intsurf1 <- normmix(ps = c(.3, .7),
+#'                     mus = list(c(0.2, 0.2), c(.8, .8)),
+#'                     sigmas = list(.01*diag(2), .01*diag(2)),
+#'                     lambda = 100,
+#'                     win = square(1))
+#' intsurf1
+#'
+normmix <- function(ps, mus, sigmas, lambda = NULL, win = NULL) {
+  if (abs(sum(ps) - 1) > .Machine$double.eps) {
     stop("Component probabilities must sum to 1.")
   }
 
@@ -38,13 +46,15 @@ normmix <- function(ps, mus, sigmas, lambda) {
     stop("Number of components mismatch.")
   }
 
-  RVAL <- list(m = length(ps), ps = ps, mus = mus, sigmas = sigmas)
-  class(RVAL) <- "normmix"
-
-  if (!missing(lambda)) {
+  if (!is.null(lambda) & spatstat::is.owin(win)) {
+    # generating intensity surface
     if (lambda <= 0) stop("Intensity must be greater than 0.")
-    RVAL <- c(RVAL, lambda = lambda)
-    class(RVAL) <- c("normmix_intensity", "normmix")
+    RVAL <- list(m = length(ps), ps = ps, mus = mus, sigmas = sigmas,
+                 intensity = lambda, window = win)
+    class(RVAL) <- c("intensity_surface", "normmix")
+  } else {
+    RVAL <- list(m = length(ps), ps = ps, mus = mus, sigmas = sigmas)
+    class(RVAL) <- "normmix"
   }
 
   RVAL
@@ -56,20 +66,46 @@ is.normmix <- function(mix) {
   ifelse(inherits(mix, "normmix"), TRUE, FALSE)
 }
 
-#' @rdname normmix
+
+is.intensity_surface <- function(mix) {
+  # check class of object, if TRUE, assuming it's created by normmix() and
+  # subject to all its constraints.
+  ifelse(inherits(mix, "intensity_surface"), TRUE, FALSE)
+}
+
 #' @export
 print.normmix <- function(mix) {
   cat("Normal Mixture with", mix$m, "component(s).\n")
 }
 
+#' @export
+print.intensity_surface <- function(mix) {
+  cat("Intensity surface with average intensity:", mix$intensity, "\n")
+  print(mix$window)
+  NextMethod("print", mix)
+}
+
+#' @param mix An object of class \code{\link{normmix}}
+#' @rdname normmix
+#' @export
+summary.normmix <- function(mix) {
+  cat(paste("Normal Mixture with", mix$m, "components:\n"))
+  for (i in 1:mix$m) {
+    cat("Component", i, "is centered at", "(",
+        round(mix$mus[[i]][1], 2), ",",
+        round(mix$mus[[i]][2], 2), ")",
+        "with probability", round(mix$ps[[i]], 4), "\n")
+  }
+}
 
 #' @rdname normmix
 #' @export
-print.normmix_intensity <- function(int_surf) {
-  cat("Intensity surface with average intensity:", int_surf$lambda, "\n",
-      "and distribution defined by: ")
-  NextMethod(int_surf)
+summary.intensity_surface <- function(mix) {
+  cat("Average number of points over window:", mix$intensity, "\n")
+  print(summary(mix$window))
+  NextMethod("print", mix)
 }
+
 
 
 #' Generate mixture with normal components.
@@ -84,22 +120,22 @@ print.normmix_intensity <- function(int_surf) {
 #' distribution.
 #' @param sigdf Degree of freedom in generating random matrix from Wishart
 #' distribution.
-#' @param win Object of class \code{spatstat::owin}. The mean vectors are
-#' inside this window.
+#' @param win An object of class \code{spatstat::owin} with default value
+#' \code{spatstat::square(1)}, The mean vectors are inside this window.
 #' @param rand_m Whether the number of components are random or fixed (default).
 #'
 #' @return Object of class \code{normmix}.
 #' @export
 #' @examples
 #' mix1 <- rnormmix(3, .01, 5, square(5))
-#' mix2 <- rnormmix(8, .01, 10, square(1), rand_m = TRUE)
+#' mix2 <- rnormmix(8, .01, 10, rand_m = TRUE)
 #'
 rnormmix <- function(m, sig0, sigdf,
-                     win, rand_m = FALSE) {
+                     win = spatstat::square(1), rand_m = FALSE) {
   if (!spatstat::is.owin(win)) {
     stop("win must be of class owin.")
   }
-  if (rand_m == TRUE) {
+  if (rand_m) {
     # number of components is random
     m <- sample(1:m, 1)
   }
@@ -116,26 +152,4 @@ rnormmix <- function(m, sig0, sigdf,
 
   normmix(ps, mus, sigmas)
 }
-
-#' @rdname normmix
-#' @export
-summary.normmix <- function(mix) {
-  cat(paste("Normal Mixture with", mix$m, "components:\n"))
-  for (i in 1:mix$m) {
-    cat(paste("Component", i, "is centered at",
-              "(", round(mix$mus[[i]][1], 2), ",",
-              round(mix$mus[[i]][2], 2), ")",
-              "with probability", round(mix$ps[[i]], 4), "\n"))
-  }
-}
-
-#' @rdname normmix
-#' @export
-summary.normmix_intensity <- function(normmix_intensity) {
-  cat("Average number of points over domain:", normmix_intensity$lambda, "\n")
-  NextMethod(normmix_intensity)
-}
-
-
-
 
