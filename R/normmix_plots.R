c3ab42c658e3dbe52b5a1bff07cedae3f4980c16
Yuchen Wang <ywang@in4ins.com>
2016-03-28 17:56:39 -0400

    mix_3d draft

--- R/normmix_plots.R
+++ R/normmix_plots.R
@@ -82,6 +82,44 @@ plot.intensity_surface <- function(intsurf, truncate = TRUE, L = 256,
 }
 
 
+plotmix_3d <- function(x, y, z, main, ...) {
+  demo_intsurf
+  win <- demo_intsurf$window
+  est_intensity <- dnormmix(demo_intsurf, xlim = win$xrange, ylim = win$yrange,
+                            L = 64)
+  est_intensity
+
+  x <- est_intensity$xcol
+  y <- est_intensity$yrow
+  z <- as.matrix(est_intensity)
+
+  # above should be passed in
+
+  jet.colors <- colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan",
+                                   "#7FFF7F", "yellow", "#FF7F00", "red",
+                                   "#7F0000"))
+
+  col <- jet.colors(100)[findInterval(z, seq(min(z), max(z), length = 100))]
+
+
+  open3d(useNULL = TRUE)  # plot at backend when calling any *3d functions
+  persp3d(x, y, z, col = col)
+
+  s <- scene3d()
+  class(s) <- c("mix_3d", class(s))
+  s
+}
+
+print.mix_3d <- function(scene, new_window = FALSE, ...) {
+  if (new_window) {
+    #     s$par3d$windowRect <- 1.5*s$par3d$windowRect
+    #     plot3d(s)
+  } else {
+    rglwidget::rglwidget(scene, ...)
+  }
+}
+
+
 #' Plot spatial point pattern generated from mixture.
 #'
 #' Plot funciton for spatial point pattern generated from mixture. It's an
